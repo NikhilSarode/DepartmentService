3b25cc9ce6bfc2c0c079cfb014f9b84a2b3607e4
NikhilSarode <nikhilsarode2974@gmail.com>
2025-05-11 19:28:38 +0530

    fixes

--- src/test/java/com/example/DepartmentAppTests.java
+++ src/test/java/com/example/DepartmentAppTests.java
@@ -1,18 +1,12 @@
-package com.example.helloworldtest;
+package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class DepartmentAppTests {
 
     @Test
-    public void contextLoads() {
-        System.out.println("Context load check.");
-    }
-
-    @Test
-    public void testHelloEndpoint() {
+    public void testDepartment() {
         System.out.println("Testing Department app done");
     }
 }
